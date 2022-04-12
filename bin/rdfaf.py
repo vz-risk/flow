e0c83790149bc3804954cc9cdae85a26c21793f5
Gabriel Bassett <gabe@infosecanalytics.com>
2022-04-12 11:39:24 -0500

    improvements to rdfaf.py

--- bin/rdfaf.py
+++ bin/rdfaf.py
@@ -1,6 +1,6 @@
 # NOTE: did not quote/unquote URIs because technically the json schema validates URIs, however this allows a LOT of room for abuse.
 # Attack Flow Version 2022-01-05-draft
-# rdfaf V1.0.0
+# rdfaf V1.0.1
 # author Gabriel Bassett
 
 from rdflib import Graph, Namespace
@@ -12,7 +12,60 @@ from collections import defaultdict
 import logging
 # may want to replace owlrl with reasonable when reasonable is more complete as owlrl is slow.
 from owlrl import DeductiveClosure, OWLRL_Semantics, RDFS_OWLRL_Semantics 
+import argparse
+import re # to split strings
+
 #import reasonable # https://lib.rs/crates/reasonable
+
+
+def updateLogger(cfg=None, formatDesign=None, dateFmt=None):
+  logger = logging.getLogger()
+  FORMAT = '%(asctime)19s - %(processName)s - %(process)d {0}- %(levelname)s - %(message)s'
+  logging_remap = {'error': logging.ERROR, 'warning':logging.WARNING, 'critical':logging.CRITICAL, 'info':logging.INFO, 'debug':logging.DEBUG,
+                   50: logging.CRITICAL, 40: logging.ERROR, 30: logging.WARNING, 20: logging.INFO, 10: logging.DEBUG, 0: logging.CRITICAL}
+  if cfg is not None:
+    log_level = logging_remap[cfg['log_level']]
+    log_file = cfg.get('log_file', None)
+  else:
+    log_level = logging.DEBUG
+    log_file = None
+  logger.setLevel(log_level)
+  handlers = logger.handlers
+  if formatDesign is None:
+    formatter = logging.Formatter(FORMAT.format(""), datefmt=dateFmt)
+  else:
+    formatter = logging.Formatter(FORMAT.format("- " + formatDesign + " "), datefmt=dateFmt)
+  streamHandlerPresent = False
+  fileHandlerPresent = False
+  for handler in handlers:
+    if type(handler) == logging.StreamHandler and streamHandlerPresent is not True:
+      handler.setLevel(log_level)
+      handler.setFormatter(formatter)
+      streamHandlerPresent = True
+    elif type(handler) == logging.FileHandler and fileHandlerPresent is not True:
+      if log_file is not None:
+        if handler.baseFilename != log_file:
+          logger.removeHandler(handler)
+        else:
+          handler.setLevel(log_level)
+          handler.setFormatter(formatter)
+          fileHandlerPresent = True
+    else:
+      logger.removeHandler(handler)
+      logging.debug("Removing handler of type {0}.".format(type(handler)))
+  if streamHandlerPresent is not True:
+    sh = logging.StreamHandler()
+    sh.setLevel(log_level)
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+    logging.debug("No stream handler found.  Adding handler.")
+  if fileHandlerPresent is not True and log_file is not None:
+    fh = logging.FileHandler(log_file)
+    fh.setLevel(log_level)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    logging.debug("No file handler found and log_file set to {0}. Adding file handler.".format(log_file))
+
 
 class flow():
     """ A class to convert attack flow to and from RDF OWL as well as validate both the JSON schema and ontological rules."""
@@ -111,7 +164,7 @@ class flow():
         # action
         actions = set()
         for action in flow_json['actions']:
-            action_id = action['id'].split("//")[-1].split("#")[-1]
+            action_id = re.split("[#/]", action['id'].split("//")[-1])[-1]
             actions.add(action_id)
             flow_graph.add((self.namespace[action_id], RDF.type, OWL.NamedIndividual))
             flow_graph.add((self.namespace[action_id], RDF.type, self.namespace.action))
@@ -124,7 +177,7 @@ class flow():
         # asset
         assets = set()
         for asset in flow_json['assets']:
-            asset_id = asset['id'].split("//")[-1].split("#")[-1]
+            asset_id = re.split("[#/]", asset['id'].split("//")[-1])[-1]
             assets.add(asset_id)
             flow_graph.add((self.namespace[asset_id], RDF.type, OWL.NamedIndividual))
             flow_graph.add((self.namespace[asset_id], RDF.type, self.namespace.asset))
@@ -139,9 +192,9 @@ class flow():
         # relationships and object properties
         for prop in ["relationships", "object_properties"]:
             for edge in flow_json[prop]:
-                source_id = edge['source'].split("//")[-1].split("#")[-1]
-                type_id = edge['type'].split("//")[-1].split("#")[-1]
-                target_id = edge['target'].split("//")[-1].split("#")[-1]
+                source_id = re.split("[#/]", edge['source'].split("//")[-1])[-1]
+                type_id = re.split("[#/]", edge['type'].split("//")[-1])[-1]
+                target_id = re.split("[#/]", edge['target'].split("//")[-1])[-1]
                 if source_id in actions:
                     if target_id in actions:
                         logging.warning("Edge <{0}, {1}, {2}> is between two actions.  This is not part of the attack flow schema and as such the edge will not be added".format(source_id, type_id, target_id))
@@ -205,8 +258,8 @@ class flow():
 
         # data properties
         for prop in flow_json['data_properties']:
-            source_id = prop['source'].split("//")[-1].split("#")[-1]
-            type_id = prop['type'].split("//")[-1].split("#")[-1]
+            source_id = re.split("[#/]", prop['source'].split("//")[-1])[-1]
+            type_id = re.split("[#/]", prop['type'].split("//")[-1])[-1]
             flow_graph.add((self.namespace[source_id], self.namespace[type_id], Literal(prop['target'])))
             flow_graph.add((self.namespace[type_id], RDF.type, OWL.DatatypeProperty))
 
@@ -218,18 +271,20 @@ class flow():
         # Flows
         # Property flows
         query = ("PREFIX  af:  <{af}>\n".format(af=self.namespace) + 
+        "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
         """SELECT DISTINCT  ?s ?o ?p
         WHERE { 
           ?s  rdf:type  af:attack-flow . 
           ?s ?o ?p .
-          ?o rdf:type owl:DatatypeProperty .
+          #?o rdf:type owl:DatatypeProperty .
+          FILTER(isLiteral(?p)) .
         }""")
         qres = flow_graph.query(query)
         flows = defaultdict(dict)
         for triple in qres:
-            flow_id = triple[0].split("#")[-1]
-            key = triple[1].split("#")[-1]
-            value = triple[2].split("#")[-1]
+            flow_id = re.split("[#/]", triple[0])[-1]
+            key = re.split("[#/]", triple[1])[-1]
+            value = re.split("[#/]", triple[2])[-1]
             flows[flow_id]['id'] = str(triple[0])
             flows[flow_id][key] = value
             flows[flow_id]["type"] = "attack-flow"
@@ -247,18 +302,20 @@ class flow():
 
         # Actions
         query = ("PREFIX  af: <{af}>\n".format(af=self.namespace) +
+        "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
         """SELECT DISTINCT  ?s ?o ?p
         WHERE { 
           ?s  rdf:type  af:action . 
           ?s ?o ?p .
-          ?o rdf:type owl:DatatypeProperty .
+          #?o rdf:type owl:DatatypeProperty .
+          FILTER(isLiteral(?p)) .
         }""")
         qres = flow_graph.query(query)
         actions = defaultdict(dict)
         for triple in qres:
-            action_id = triple[0].split("#")[-1]
-            key = triple[1].split("#")[-1]
-            value = triple[2].split("#")[-1]
+            action_id = re.split("[#/]", triple[0])[-1]
+            key = re.split("[#/]", triple[1])[-1]
+            value = re.split("[#/]", triple[2])[-1]
             actions[action_id]['id'] = str(triple[0])
             actions[action_id][key] = value
         query = ("PREFIX  af:  <{af}>\n".format(af=self.namespace) +
@@ -268,24 +325,26 @@ class flow():
         }""")
         qres = flow_graph.query(query)
         for action in qres:
-            action_id = action[0].split("#")[-1]
+            action_id = re.split("[#/]", action[0])[-1]
             actions[action_id]['id'] = str(action[0])
         actions = [v for k,v in actions.items()]
 
         # Assets
         query = ("PREFIX  af:  <{af}>\n".format(af=self.namespace) +
+        "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
         """SELECT DISTINCT  ?s ?o ?p
         WHERE { 
           ?s  rdf:type  af:asset . 
           ?s ?o ?p .
-          ?o rdf:type owl:DatatypeProperty .
+          #?o rdf:type owl:DatatypeProperty .
+          FILTER(isLiteral(?p)) .
         }""")
         qres = flow_graph.query(query)
         assets = defaultdict(dict)
         for triple in qres:
-            asset_id = triple[0].split("#")[-1]
-            key = triple[1].split("#")[-1]
-            value = triple[2].split("#")[-1]
+            asset_id = re.split("[#/]", triple[0])[-1]
+            key = re.split("[#/]", triple[1])[-1]
+            value = re.split("[#/]", triple[2])[-1]
             assets[asset_id]['id'] = str(triple[0])
             assets[asset_id][key] = value        
         query = ("PREFIX  af:  <{af}>\n".format(af=self.namespace) +
@@ -295,7 +354,7 @@ class flow():
         }""")
         qres = flow_graph.query(query)
         for asset in qres:
-            asset_id = asset[0].split("#")[-1]
+            asset_id = re.split("[#/]", asset[0])[-1]
             assets[asset_id]['id'] = str(asset[0])  
 ### for if state is an object property rather than a data property
 #        qres = flow_graph.query(("PREFIX  af:  <{af}>\n".format(af=self.namespace) +
@@ -306,7 +365,7 @@ class flow():
 #                }"""))
 #        qres2 = defaultdict(list)
 #        for tupl in qres:
-#            qres2[tupl[0].split("#")[-1]].append(str(tupl[1]))
+#            re.split("[#/]", qres2[tupl[0])[-1]].append(str(tupl[1]))
 #        for asset in qres2:
 #            # NOTE: the below only adds the first state URI. State should probably be moved to a manditory property like 'flow'.
 #            assets[asset]['state'] = qres2[asset][0] 
@@ -326,9 +385,9 @@ class flow():
         object_properties = []
         qres = flow_graph.query(query)
         for triple in qres:
-            source = str(triple[0]) # .split("#")[-1]
-            type_ = str(triple[1]) # .split("#")[-1]
-            target = str(triple[2]) # .split("#")[-1]
+            source = str(triple[0]) # )[-1]
+            type_ = str(triple[1]) # )[-1]
+            target = str(triple[2]) # )[-1]
             object_properties.append({
                 "source": source,
                 "type": type_,
@@ -337,19 +396,21 @@ class flow():
 
         # Data Properties
         query = ("PREFIX  af:  <{af}>\n".format(af=self.namespace) +
+        "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" +
         """SELECT DISTINCT  ?s ?o ?p
          WHERE {
           {
-            ?o rdf:type owl:DatatypeProperty .
             ?s ?o ?p 
+            #?o rdf:type owl:DatatypeProperty .
+            FILTER(isLiteral(?p)) .
           } 
         }""")
         data_properties = []
         qres = flow_graph.query(query)
         for triple in qres:
-            source = str(triple[0]) # .split("#")[-1]
-            type_ = str(triple[1]) # .split("#")[-1]
-            target = str(triple[2]) # .split("#")[-1]
+            source = str(triple[0]) # )[-1]
+            type_ = str(triple[1]) # )[-1]
+            target = str(triple[2]) # )[-1]
             data_properties.append({
                 "source": source,
                 "type": type_,
@@ -477,7 +538,7 @@ class flow():
             isLiteral(?p)
           }
         }"""
-### FOr where state is an object property rather than a data property
+### For where state is an object property rather than a data property
 #        where = """{
 #          {
 #            ?s af:state ?p .
@@ -502,3 +563,56 @@ class flow():
         ### OWL-RL
         DeductiveClosure(RDFS_OWLRL_Semantics).expand(self.flow_rdf)
 
+
+
+def main(cfg):
+    case = flow()
+
+    # load the namespace
+    case.load_namespace(cfg['ns'])
+
+    # load the json schema
+    with open(cfg['schema'], 'r') as filehandle:
+        case.load_schema(json.load(filehandle))
+
+    # load the rdf
+    rules = Graph()
+    if 'ld' in cfg:
+        rules.parse(cfg['ld'], format="json-ld")
+    else:
+        rules.parse(cfg['ns'], format="json-ld")
+    case.load_rules(rules)
+
+    # load the case
+    with open(cfg['input'], 'r') as filehandle:
+        in_case = json.load(filehandle)
+    if type(in_case) == list:
+        in_case_j = in_case
+        in_case = Graph()
+        in_case.parse(data=json.dumps(in_case_j), format="json-ld")
+    elif type(in_case) == dict:
+        pass # json schema so can just pass
+    case.load_flow(in_case)
+
+    # write output
+    with open(cfg['output'], 'w') as filehandle:
+        filehandle.write(case.get_flow_graph(rules=False).serialize(format="json-ld"))
+
+
+if __name__ == "__main__":
+
+    # Parse Arguments (should correspond to user variables)
+    parser = argparse.ArgumentParser(description='This script takes a schema labels file and a file of updates to it and adds the updates to the original file.')
+    parser.add_argument("-l","--log_level",choices=["critical","warning","info","debug"], help="Minimum logging level to display", default="warning")
+    parser.add_argument('--log_file', help='Location of log file')
+    parser.add_argument('--ns', help='The namespace for attack flow', default="https://vz-risk.github.io/flow/attack-flow/")
+    parser.add_argument('--ld', help='The atack flow ontology as linked data.  If not supplied, an attempt will be made to load it from the namespace.')
+    parser.add_argument('-s', '--schema', required=True, help='The attack flow JSON schema.')
+    parser.add_argument('-i', '--input', required=True, help='The labels file to be updated.')
+    parser.add_argument('-o', '--output', required=True, help='The labels file to be outputted.')
+    args = parser.parse_args()
+    args = {k:v for k,v in vars(args).items() if v is not None}
+
+    updateLogger(args)
+
+    main(args)
